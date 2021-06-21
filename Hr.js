3a19ea7e98fe373a717fbf3bb6593aac0fd8781b
SHIVANI SANTOSH UPADHYAY <76774000+shivani0986@users.noreply.github.com>
2021-06-21 14:06:18 +0530

    Add files via upload

---
+++ Hr.js
@@ -0,0 +1,28 @@
+class Hr {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':1,
+          'density':5,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+
+      this.image = loadImage("mum.png-removebg-preview.png");
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0,0,this.width, this.height);      
+      pop();
+    }
+  }
